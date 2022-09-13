007d5779d41535da412fb3c6f645a8b79156e67d
alibhz <pleasdonthack78@gmail.com>
2022-09-13 20:03:41 +0430

    fly

---
+++ src/js/changelg.js
@@ -0,0 +1,44 @@
+const persain=()=>{
+    document.getElementById("maincontainer").dir = "rtl";
+
+    document.getElementById("searchfly").innerText="جستجو"
+
+    document.getElementById("Flight").innerText="فلای لیدی"
+
+    document.getElementById("Home").innerText="خانه"
+
+    document.getElementById("About").innerText="پشتیبانی"
+
+    document.getElementById("Support").innerText="درباره ما"
+
+    document.getElementById("Connect").innerText="تماس با ما"
+
+
+    document.getElementById("login").innerText="ورود"
+
+    document.getElementById("signup").innerText="ثبت نام"
+
+    document.getElementById("Flightlady").innerText="فلای لیدی"
+
+    document.getElementById("flsh1").dir = "rtl";
+
+    document.getElementById("searchfly").innerText="جستجو"
+
+    document.getElementById("searchfly").innerText="جستجو"
+
+    document.getElementById("searchfly").innerText="جستجو"
+
+    document.getElementById("searchfly").innerText="جستجو"
+
+    document.getElementById("searchfly").innerText="جستجو"
+
+    document.getElementById("searchfly").innerText="جستجو"
+
+    document.getElementById("searchfly").innerText="جستجو"
+
+    document.getElementById("searchfly").innerText="جستجو"
+
+    document.getElementById("searchfly").innerText="جستجو"
+
+    document.getElementById("searchfly").innerText="جستجو"
+}
